3673149b0a889338bdc1d08800178d78dcf616a7
CaioDavid01 <161221118+CaioDavid01@users.noreply.github.com>
2024-04-13 20:41:33 -0300

    01

--- CAP-04/Exercicios-Resolvidos/QR05/src/src/br/edu/principal/Principal.java
+++ CAP-04/Exercicios-Resolvidos/QR05/src/src/br/edu/principal/Principal.java
@@ -1,10 +1,50 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner scanner = new Scanner(System.in);
+		
+		float num1, num2, num3, num4;
+		
+		System.out.println(" O primeiro numero é ");
+		num1 = scanner.nextFloat();
+		
+		System.out.println(" O segundo numero é ");
+		num2 = scanner.nextFloat();
+		
+		System.out.println(" O terceiro numero é ");
+		num3 = scanner.nextFloat();
+		
+		System.out.println(" O quarto numero é ");
+		num4 = scanner.nextFloat();
+		
+		
+		
+		if(num4 > num3) {
+			System.out.println(" A ordem decrescente é: " + num4 + "-" + num3 + "-" + num2 + "-" + num1);
+			
+		}
+		if(num4 > num2 && num4 < num3) {
+			System.out.println(" A ordem decrescente é: " +  num3 + "-" + num4 + "-" + num2 + "-" + num1);
+			
+			
+		}
+		
+		if(num4 > num1 && num4 < num2) {
+			System.out.println(" A ordem decrescente é: " +  num3 + "-" + num2 + "-" + num4 + "-" + num1);
+		}
+		
+		if(num4 < num1) {
+			System.out.println(" A ordem decrescente é: " +  num3 + "-" + num2 + "-" + num1 + "-" + num4);	
+		}
+			
+		
+		
 
 	}
 
 }
+	
