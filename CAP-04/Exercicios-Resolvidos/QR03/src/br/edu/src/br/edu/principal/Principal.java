592688e3a3eb2fcca4acc3dc5d40d7bdcbcf2a0b
CaioDavid01 <161221118+CaioDavid01@users.noreply.github.com>
2024-04-13 20:44:37 -0300

    02

--- CAP-04/Exercicios-Resolvidos/QR03/src/br/edu/src/br/edu/principal/Principal.java
+++ CAP-04/Exercicios-Resolvidos/QR03/src/br/edu/src/br/edu/principal/Principal.java
@@ -1,10 +1,41 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		float num1,num2;
+		
+		System.out.println("Digite o primeiro numero.");
+		num1 = scanner.nextFloat();
+		
+		System.out.println("Digite o segundo numero.");
+		num2 = scanner.nextFloat();
+		
+		
+		
+		if(num1 > num2) {
+			System.out.println( " O " + num1  + " é maior ");
+			
+		}
+		
+		else if(num2 > num1) {
+			System.out.println(" O " +  num2 +  " é maior ");
+				
+			
+		}
+		
+		
+		else {
+			System.out.println(" Os dois são iguais ");
+		}
 	}
+	
+	
+	
 
 }
+ 
